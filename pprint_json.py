de7f416a01a1b9d8803bb64e3c94088319c619ee
Alt13 <Alt90@rambler.ru>
2016-09-30 08:38:31 +0300

    Реализовал вывод содержимого json файла в консоль

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
+import sys
+
+from pprint import pprint
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as data_file:
+        return json.load(data_file)
 
 
 def pretty_print_json(data):
-    pass
+    pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    if (len(sys.argv) < 2):
+        print("File don`t enter. Please run script with argv.")
+        sys.exit()
+    data = load_data(sys.argv[1])
+    pretty_print_json(data)
